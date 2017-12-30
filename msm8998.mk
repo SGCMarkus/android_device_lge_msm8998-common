b624d0b96dcb616bb5f00717f2f194bb61842779
SGCMarkus <markusornik@gmail.com>
2017-12-30 16:08:55 +0100

    removed rest of ANT tries...

--- msm8998.mk
+++ msm8998.mk
@@ -108,10 +108,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
-
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     hwaddrs
