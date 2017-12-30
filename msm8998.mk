61df52df749a44a5bbab867b645d6397fc830f9d
SGCMarkus <markusornik@gmail.com>
2017-12-30 12:46:09 +0100

    updates...

--- msm8998.mk
+++ msm8998.mk
@@ -353,7 +353,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 # ADB crap
